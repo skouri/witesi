20ee9baefae3687c61b3919d6fa3acd3dde75a4b
skouri <skouri@yahoo.com>
2019-02-20 14:21:26 -0600

    Made better use of Contract's state.

--- src/ESI.js
+++ src/ESI.js
@@ -27,43 +27,6 @@ class ESI {
         }
     }
 
-    /* Station
-        {
-        "max_dockable_ship_volume": 50000000,
-        "name": "Sendaya V - CONCORD Bureau",
-        "office_rental_cost": 2029825,
-        "owner": 1000125,
-        "position": {
-            "x": -235663564800,
-            "y": 14395269120,
-            "z": -83101655040
-        },
-        "race_id": 4,
-        "reprocessing_efficiency": 0.5,
-        "reprocessing_stations_take": 0.05,
-        "services": [
-            "bounty-missions",
-            "courier-missions",
-            "interbus",
-            "reprocessing-plant",
-            "market",
-            "stock-exchange",
-            "cloning",
-            "repair-facilities",
-            "fitting",
-            "news",
-            "insurance",
-            "docking",
-            "office-rental",
-            "loyalty-point-store",
-            "navy-offices",
-            "security-offices"
-        ],
-        "station_id": 60012301,
-        "system_id": 30000015,
-        "type_id": 1932
-        }
-    */
     static async getStation(stationId) {
         try {
             const response = await fetch(`https://esi.evetech.net/latest/universe/stations/${stationId}/?datasource=tranquility`);
@@ -81,6 +44,20 @@ class ESI {
     static async getSystem(systemId) {
         try {
             const response = await fetch(`https://esi.evetech.net/latest/universe/systems/${systemId}/?datasource=tranquility&language=en-us`);
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+    
+            return response.json();
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async getCharacter(characterId) {
+        try {
+            const response = await fetch(`https://esi.evetech.net/latest/characters/${characterId}/?datasource=tranquility`);
             if (!response.ok) {
                 throw Error(response.statusText);
             }
--- src/components/Contract.js
+++ src/components/Contract.js
@@ -1,33 +1,47 @@
 import React, { Component } from 'react';
 import ESI from '../ESI';
+import moment from 'moment';
 import './Contract.css';
 import { Container, Row, Col } from 'react-bootstrap';
 
 class Contract extends Component {
   constructor() {
     super();
-    this.station = {};
-    this.system = {};
+    this.state = { 
+      character: {},
+      station: {},
+      system: {}
+    };
   }
 
   async componentDidMount() {
-    this.station = await ESI.getStation(this.props.details.end_location_id);
-    this.system = await ESI.getSystem(this.station.system_id);
+    console.log("CDM start");
+    let station = await ESI.getStation(this.props.details.end_location_id);
+    let system = await ESI.getSystem(station.system_id);
+    let character = await ESI.getCharacter(this.props.details.issuer_id);
+    this.setState({ 
+      character: character,
+      station: station,
+      system: system
+     });
   }
 
   render() {
+    let now = moment.now();
+    let expired = moment(this.props.details.date_expired);
+    let timeLeft = moment.duration(expired.diff(now)).humanize(true);
     return (
       <div className="Contract">
         <Container>
           <Row>
             <Col>{this.props.details.title}</Col>
-            <Col>{this.system.name}</Col>
+            <Col>{this.state.system.name}</Col>
             <Col>{this.props.details.price}</Col>
-            <Col>Jumps TBD</Col>
-            <Col>Time Left TBD</Col>
-            <Col>{this.props.issuer_id}</Col>
+            {/* TODO <Col>Jumps TBD</Col> */}
+            <Col>{ timeLeft }</Col>
+            <Col>{this.state.character.name}</Col>
             <Col>{this.props.details.date_issued}</Col>
-            <Col>Info by Issuer?</Col>
+            {/* TODO <Col>Info by Issuer?</Col> */}
           </Row>
         </Container>
       </div>
